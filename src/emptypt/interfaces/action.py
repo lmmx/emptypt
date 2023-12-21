f80e0b8144345c1dacc21475fa05dae3b5637fe7
Louis Maddox <louismmx@gmail.com>
2023-12-21 18:41:32 +0000

    refactor(argh-cli): rename the `field` helper func to `desc` [msgspec API clash]

--- src/emptypt/interfaces/action.py
+++ src/emptypt/interfaces/action.py
@@ -5,7 +5,7 @@ from msgspec import Meta, Struct
 __all__ = ("ActionConfig",)
 
 
-def field(typ, description: str):
+def desc(typ, description: str):
     """Annotate a `msgspec.Struct` field with a description"""
     return Annotated[typ, Meta(description=description)]
 
@@ -13,8 +13,8 @@ def field(typ, description: str):
 class ActionConfig(Struct):
     """Configure input filtering and output display."""
 
-    io_arg1: field(bool, "Example IO flag") = False
-    filter_arg1: field(bool, "Example filter flag") = False
-    quiet: field(bool, "Run silently") = False
-    debug: field(bool, "Run debug diagnostics") = False
+    io_arg1: desc(bool, "Example IO flag") = False
+    filter_arg1: desc(bool, "Example filter flag") = False
+    quiet: desc(bool, "Run silently") = False
+    debug: desc(bool, "Run debug diagnostics") = False
     undocumented: bool = False
